eae37950cd9d197dce263117c11780377cd7aeb1
MaximKarlov <111450676+MaximKarlov@users.noreply.github.com>
2022-11-04 08:49:57 +0000

    Deploying to gh-pages from @ MaximKarlov/FoodSTA_UTF-8@afeb697629769add98a1736db83fdbb5186c70d2 🚀

---
+++ index.7adb152d.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),openModalBtn2:document.querySelector("[data-modal-nepo]"),openModalBtn3:document.querySelector("[data-modal-peon]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.openModalBtn2.addEventListener("click",o),e.openModalBtn3.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.7adb152d.js.map
